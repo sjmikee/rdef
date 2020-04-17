2185375e5d1fb7a2636408b36761cf25a9db868c
sjmikee <sjmikee@gmail.com>
2020-04-17 11:42:32 +0300

    server folder added

---
+++ server/threading_http_server.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+import ssl
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+
+
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+    address_family = socket.AF_INET6
+    daemon_threads = True
+
+    def handle_error(self, request, client_address):
+        # surpress socket/ssl related errors
+        cls = sys.exc_info()[:2]
+        if cls is socket.error or cls is ssl.SSLError:
+            pass
+        else:
+            return HTTPServer.handle_error(self, request, client_address)
